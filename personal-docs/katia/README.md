b08f013078d11e5321c5101d9f16c985c13f4e8b
Mekhova Kateryna <katrindeutschland33333@gmail.com>
2024-03-08 14:37:10 +0100

    Finnished the banner. Created media for 1000, 768, 480, 360 px

---
+++ personal-docs/katia/README.md
@@ -0,0 +1,5 @@
+8.03
+Finnished the banner
+Created media for 1000, 768, 480, 360 px
+Pherhaps need to fix text so he does not jump
+Mayby make the distance between the button and the text more correct
--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -4,10 +4,12 @@ import classes from "./Banner.module.css";
 export const Banner = () => {
   return (
     <div className={classes.container}>
-      <h1 className={classes.text_amazing__discounts}>
-        Amazing Discounts on Garden Products!
-      </h1>
-      <button className={classes.button_check}>Check out</button>
+      <div className={classes.context_container}>
+        <h1 className={classes.text_amazing__discounts}>
+          Amazing Discounts on Garden Products!
+        </h1>
+        <button className={classes.button_check}>Check out</button>
+      </div>
     </div>
   );
 };
